5e30d0804e5a50280c8d8bb13f59a36d3e760bad
mikasajaeger19 <naeemniaz2012@gmail.com>
2024-02-11 17:57:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// //import openai from './config/open-ai.js';
+// import readlineSync from 'readline-sync';
+// import colors from 'colors';
+
+// async function main() {
+//     const userName = readlineSync.question('What is your name?');
+//     console.log(`Hello, ${userName}!`.green);
+// }
+
+// main();
+
+// console.log("hello")
+
+
+import readlineSync from 'readline-sync';
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
