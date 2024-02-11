682acf29bbd81c6ca231c5cff30b0203b9106a22
mikasajaeger19 <naeemniaz2012@gmail.com>
2024-02-11 19:46:19 +0530

    finished

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-// //import openai from './config/open-ai.js';
-// import readlineSync from 'readline-sync';
-// import colors from 'colors';
+import openai from './config/open-ai.js';
+import readlineSync from 'readline-sync';
+import colors from 'colors';
 
-// async function main() {
-//     const userName = readlineSync.question('What is your name?');
-//     console.log(`Hello, ${userName}!`.green);
-// }
+async function main() {
+    const userName = readlineSync.question('What is your name?\n');
+    console.log(`Hello, ${userName}!\n`);
 
-// main();
+    while(true){
+        console.log('Ask me a question or type "exit" to leave\n');
+        const userInput = readlineSync.question('You:'.yellow);
+
+        if(userInput === "exit"){
+            console.log("\nGoodbye!".red);
+            break;
+        }
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ "role": 'user', "content": userInput }],
+            max_tokens: 100
+        }).then((response) => {
+            //console.log(response.choices[0].message.content.yellow);
+            console.log(`chatteuBotteu: ${response.choices[0].message.content}`.green);
+        }).catch((error) => {
+            console.error('Error:', error);
+        })
+    }
+
+
+}
+
+
+main();
 
 // console.log("hello")
 
 
-import readlineSync from 'readline-sync';
- 
-// Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
  
